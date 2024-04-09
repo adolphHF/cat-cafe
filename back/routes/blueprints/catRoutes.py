eb5120ddb89e17bdf7c5c2009ab016166be1e803
adolphHF <adolhdzf@gmail.com>
2024-04-09 12:01:50 -0600

    import repository

--- back/routes/blueprints/catRoutes.py
+++ back/routes/blueprints/catRoutes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 #here we should import the queries from repository
+import back.repository.cat.repository.py
 
 catRoutes = Blueprint("catRoutes", __name__, template_folder="blueprints")
 
