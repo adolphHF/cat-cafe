5ac8a514b668732a356c1350496fd35051f9f814
adolphHF <adolhdzf@gmail.com>
2024-04-12 10:04:00 -0600

    user routes added to main

--- back/main.py
+++ back/main.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 from routes.blueprints.catRoutes import catRoutes
+from routes.blueprints.userRoutes import userRoutes
 
 app = Flask(__name__)
 app.register_blueprint(catRoutes, url_prefix ="/cats")
+app.register_blueprint(userRoutes, url_prefix ="/user")
 
 @app.route("/")
 def test():
---
+++ back/repository/user.repository.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.insert(0, 'C:/Users/adolh/OneDrive/Escritorio/cat-cafe/back/database')
+#Luisen Option sys.path.append('../database')
+
+from connectionPool import connectionPool
+
+conn = connectionPool.getConnection()
+
+try:
+    # Use the connection
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = cursor.fetchall()
+    print(tables)
+finally:
+    # Release the connection back to the pool
+    connectionPool.releaseConnection(conn)
+
+def getUser(id, password):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM user WHERE id = ?, password = ?", [id, password])
+        user = cursor.fetchall()
+        return user
+    finally:
+        connectionPool.releaseConnection(conn)
